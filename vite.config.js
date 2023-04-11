4f7a78365285a8eb044392361970dce4b4593ef9
Mehdi Mahmoudi <mahmoudi.web.dev@gmail.com>
2023-04-11 10:36:13 +0330

    refactor: cleanup vite default

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src/partials"),
+    })
+  ],
+};
